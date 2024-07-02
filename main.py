cf8f837c4774aa34f991f55432bec2bda749b031
realharshita <slayyharshita@gmail.com>
2024-07-02 14:38:02 +0530

    levels

--- main.py
+++ main.py
@@ -26,30 +26,53 @@ ball_y = screen_height // 2
 brick_width = 75
 brick_height = 20
 brick_color = (255, 0, 0)
-brick_rows = 5
-brick_cols = 10
 brick_padding = 10
-bricks = []
-
-for row in range(brick_rows):
-    brick_row = []
-    for col in range(brick_cols):
-        brick_x = col * (brick_width + brick_padding) + brick_padding
-        brick_y = row * (brick_height + brick_padding) + brick_padding
-        brick_rect = pygame.Rect(brick_x, brick_y, brick_width, brick_height)
-        brick_row.append(brick_rect)
-    bricks.append(brick_row)
-
 power_up_width = 20
 power_up_height = 20
 power_up_color = (0, 255, 0)
 power_up_speed = 5
-power_ups = []
+
+levels = [
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ],
+    [
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+    ],
+    [
+        [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
+    ]
+]
+
+current_level = 0
+bricks = []
+
+def load_level(level):
+    global bricks
+    bricks = []
+    for row_index, row in enumerate(level):
+        brick_row = []
+        for col_index, brick in enumerate(row):
+            if brick == 1:
+                brick_x = col_index * (brick_width + brick_padding) + brick_padding
+                brick_y = row_index * (brick_height + brick_padding) + brick_padding
+                brick_rect = pygame.Rect(brick_x, brick_y, brick_width, brick_height)
+                brick_row.append(brick_rect)
+        bricks.append(brick_row)
 
 def create_power_up(x, y):
     power_up_type = random.choice(["paddle_increase", "extra_ball"])
     power_up_rect = pygame.Rect(x, y, power_up_width, power_up_height)
     return {"rect": power_up_rect, "type": power_up_type}
+
+load_level(levels[current_level])
+power_ups = []
 
 running = True
 while running:
@@ -84,7 +107,7 @@ while running:
         ball_speed_y = -ball_speed_y
 
     for row in bricks:
-        for brick in row:
+        for brick in row[:]:
             if ball_rect.colliderect(brick):
                 ball_speed_y = -ball_speed_y
                 row.remove(brick)
@@ -101,6 +124,12 @@ while running:
             power_ups.remove(power_up)
         elif power_up["rect"].y > screen_height:
             power_ups.remove(power_up)
+
+    if all(len(row) == 0 for row in bricks):
+        current_level += 1
+        if current_level >= len(levels):
+            current_level = 0
+        load_level(levels[current_level])
 
     screen.fill((0, 0, 0))
 
